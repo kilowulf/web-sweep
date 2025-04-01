804b98345fc8af21ea5bf52facf7633e566b7615
Aaron A. Brown <kilonow1275@hotmail.com>
2025-04-01 00:13:40 -0600

    added regex for proper url assignment

--- lib/helper/appUrl.ts
+++ lib/helper/appUrl.ts
@@ -6,7 +6,14 @@
  * @param {string} path - The relative path to append to the base URL.
  * @returns {string} The full URL constructed from the base URL and the provided path.
  */
+
+// old version
+// export function getAppUrl(path: string) {
+//   const appUrl = process.env.NEXT_PUBLIC_APP_URL;
+//   return `${appUrl}/${path}`;
+// }
+
 export function getAppUrl(path: string) {
-  const appUrl = process.env.NEXT_PUBLIC_APP_URL;
-  return `${appUrl}/${path}`;
+  const appUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+  return `${appUrl}/${path}`.replace(/([^:]\/)\/+/g, "$1");
 }
