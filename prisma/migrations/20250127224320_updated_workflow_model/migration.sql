de49c05792bd3f9126a307e5dce9e324ddb61aaf
Aaron A. Brown <kilonow1275@hotmail.com>
2025-03-31 22:28:45 -0600

    delete prisma migration and local db files. update tsconfig path to "./src/*" from "./*"

--- prisma/migrations/20250127224320_updated_workflow_model/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "WorkFlow" ADD COLUMN "lastRunAt" DATETIME;
-ALTER TABLE "WorkFlow" ADD COLUMN "lastRunId" TEXT;
-ALTER TABLE "WorkFlow" ADD COLUMN "lastRunStatus" TEXT;
--- prisma/migrations/20250216230830_added_cron_to_workflow_model/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "WorkFlow" ADD COLUMN "cron" TEXT;
-ALTER TABLE "WorkFlow" ADD COLUMN "nextRunAt" DATETIME;
